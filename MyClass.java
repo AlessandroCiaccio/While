5960759616a6e5aebde335385614deb3b861a21a
Alessandro <sandroscro@libero.it>
2023-10-12 19:54:26 +0200

    Added Main

---
+++ MyClass.java
@@ -0,0 +1,11 @@
+public class MyClass {
+    public static void main(String args[]) {
+      int x=5, i=0, sum=0;
+      while (i<x){
+          i++;
+          sum+=i;
+      }
+      System.out.println("The sum is: "+sum);
+    }
+    
+}
