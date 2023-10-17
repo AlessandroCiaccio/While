d38289dc06ccb16e3c7ffc78a52ac7e8555da73f
Alessandro <sandroscro@libero.it>
2023-10-17 12:20:09 +0200

    Aggiunto metodo

--- MyClass.java
+++ MyClass.java
@@ -1,11 +1,16 @@
 public class MyClass {
     public static void main(String args[]) {
-      int x=5, i=0, sum=0;
-      while (i<x){
-          i++;
-          sum+=i;
-      }
-      System.out.println("The sum is: "+sum);
+      int x=6;
+      somma(x);
+    }
+    public static void somma(int x){
+        int i=0, sum=0;
+        while (i<x){
+            sum+=i;
+            i++;
+        }
+        System.out.println("The sum is: "+sum);
+
     }
     
 }
